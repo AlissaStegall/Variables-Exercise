165f0a8b4da7138c673b939f07e670a8da997fbc
AlissaStegall <alissaistegall@gmail.com>
2023-04-10 15:48:48 -0700

    declared and initialized variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Alissa";
+            int age = 28;
+            char middleInitial = 'I';
+            bool over21YearsOld = true;
+            double currentTemp = 100;
+            decimal currentGasPrice = 4.95m;
+
+            Console.WriteLine($"Hello, World! My name is {name} I am {age} years old.");
+            Console.WriteLine($"Which means it is {over21YearsOld} I am over the age of 21");
+            Console.WriteLine($"My middle name starts with an {middleInitial} for Irene.");
+            Console.WriteLine($"I am from Arizona where the current temp is {currentTemp} degrees fahrenheit");
+            Console.WriteLine($"Gas prices are sky high here, currently at {currentGasPrice} dollars per gallon");
         }
+
     }
+
 }
